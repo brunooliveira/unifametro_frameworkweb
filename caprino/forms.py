0ce97b735cab0864c97060d9e3bd81ddae903daf
Bruno Oliveira <brunnooliver@gmail.com>
2023-03-06 15:48:16 -0300

    add caprino app

---
+++ caprino/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from .models import Ovino
+
+
+class OvinoForm(forms.ModelForm):
+
+	class Meta:
+		model = Ovino
+
+		fields = [
+			"identificador",
+            "peso",
+            "nascimento",
+            "raca",
+            "cor",
+            "genero",
+            "tag",
+		]
